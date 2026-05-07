5edf4c5d61d6ba2cc1ddd5f9ed6ef634ecec8c98
Amina CHELIGHEM <amina.chelighem.etu@univ-lille.fr>
2026-05-07 09:42:13 +0200

    ajouter (copie) apres le label du groupe dans le module planity

--- dolibarr-src/custom/monday/main.php
+++ dolibarr-src/custom/monday/main.php
@@ -265,8 +265,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST'
                          FROM llx_myworkspace_group
                         WHERE fk_workspace = ".$targetWorkspaceId);
     $position = ($res && $row = $db->fetch_object($res)) ? ((int) $row->m + 1) : 0;
-
-    $groupLabel = $db->escape($sourceGroup->label);
+    $groupLabel = $db->escape($sourceGroup->label . ' (copie)');
     $taskColumnLabel = $db->escape($sourceGroup->task_column_label ?: 'Tâche');
 
     $ok = $db->query("INSERT INTO llx_myworkspace_group
