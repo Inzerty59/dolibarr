f330bb46f93589be5409821be9ca5cecd6d96953
Amina CHELIGHEM <amina.chelighem.etu@univ-lille.fr>
2026-06-10 13:53:15 +0200

    correction des bugs

--- custom/tickets/global_intercept.php
+++ custom/tickets/global_intercept.php
@@ -2,14 +2,16 @@
 
 if (strpos($_SERVER['SCRIPT_NAME'] ?? '', 'ticket/card.php') !== false) {
 	global $conf;
-
+	
 	if (!empty($conf->tickets->enabled)) {
 		$action = $_GET['action'] ?? $_POST['action'] ?? '';
 		$fk_project = (int)($_GET['fk_project'] ?? $_POST['fk_project'] ?? 0);
-
+		
+		if ($action === 'create' && $fk_project === 0) {
 		if ($action === 'create' && $fk_project === 0) {
 			header('Location: '.DOL_URL_ROOT.'/custom/tickets/select_project.php', true, 302);
 			exit(0);
 		}
 	}
+}
 }
