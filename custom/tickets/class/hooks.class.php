990c8fd300a6a3d0f813f20752b8fecf61b68444
Amina CHELIGHEM <amina.chelighem.etu@univ-lille.fr>
2026-07-20 16:15:33 +0200

    fix: update hooks.class.php

--- custom/tickets/class/hooks.class.php
+++ custom/tickets/class/hooks.class.php
@@ -1,16 +1,21 @@
 <?php
+/*
+ * T6 - Hook class alias.
+ *
+ * Dolibarr loads hook classes by module naming convention. This lightweight
+ * class exposes ActionsTickets under the expected custom module hook class
+ * while keeping the real implementation in actions_tickets.class.php.
+ */
 
-class ActionsTickets
+require_once __DIR__.'/actions_tickets.class.php';
+
+class TicketsHooks extends ActionsTickets
 {
+
 	public $error = '';
 	public $errors = array();
 	public $resprints = '';
 	public $results = array();
-
-	public function __construct($db)
-	{
-		$this->db = $db;
-	}
 
 	public function addMoreActionsButtons($parameters, &$object, &$action, $hookmanager)
 	{
